44de6ff02e65bd73ea23c3499b280936fc75edbd
Bex-G <bexgiacinto@gmail.com>
2023-12-29 17:56:31 -0800

    Create 'close' btn to remove list items.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-// content
+// Create content structure
 
 const content = document.getElementById('content');
 
 const div = document.createElement('div');
-div.setAttribute('id', 'div');
+div.id = 'div';
 
     const input = document.createElement('input');
     input.type = 'text';
@@ -12,7 +12,7 @@ div.setAttribute('id', 'div');
 
     const addBtn = document.createElement('button');
     addBtn.type = 'submit';
-    addBtn.innerHTML = 'submit'
+    addBtn.innerHTML = 'add'
     addBtn.addEventListener("click", newListItem);
     div.appendChild(addBtn);
 
@@ -22,12 +22,31 @@ ul.id = 'myList'
 content.appendChild(div);
 content.appendChild(ul);
 
-// functions
+// Create a "close" button and append it to each list item
+let myNodelist = document.getElementsByTagName("li");
+for (i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+    }
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+    }
+}
+
+// Create a new list item when clicking on the "Add" button
 
 function newListItem() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
         alert("You must write something!");
@@ -35,4 +54,17 @@ function newListItem() {
         document.getElementById("myList").appendChild(li);
       }
       document.getElementById("myInput").value = "";
+
+      let span = document.createElement("span");
+      let txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
+    
+      for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          let div = this.parentElement;
+          div.style.display = "none";
+        }
+    }
 }
