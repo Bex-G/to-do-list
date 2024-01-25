53a6f94f3c18b0181c1cf71cc3288850c39a5deb
Bex-G <bexgiacinto@gmail.com>
2024-01-24 16:06:42 -0800

    Create 'counter' variable to set unique ids and enable toggleable tab display.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import {format} from 'date-fns';
 
-function List(name, note, date, priority) {
-    this.name = name;
-    this.note = note;
-    this.date = 'Due: ' + format(date, 'PP');
-    this.priority = priority;
-}
-
-const content = document.getElementById('content');
 const sidebar = document.getElementById('sidebar');
-const sidebarUl = document.getElementById('sidebarUl');
-const main = document.getElementById('main');
-
-// sidebar functions
+const listContainer = document.getElementById('listContainer');
 
 document.getElementById('newListBtn').addEventListener('click', (e) => {
     e.preventDefault();
@@ -23,23 +12,28 @@ document.getElementById('newListBtn').addEventListener('click', (e) => {
 
 function openForm() {
     document.getElementById("myForm").style.display = 'block';
-}
+};
 
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
-}  
+};
 
 function resetForm() {
     document.getElementById('nameInput').value = null;
     document.getElementById('noteInput').value = null;
     document.getElementById('dateInput').value = null;
     document.querySelector('input[name="priorityInput"]').value = null;
-}
+};
+
+let counter = 0;
 
 document.getElementById('submitBtn').addEventListener('click', (e) => {
     e.preventDefault();
-    newList();
-    addToSidebar();
+    counter++
+    addTab(counter);
+    addList(counter);
+    addToSidebar(counter);
+    openTab(counter);
     resetForm();
     closeForm();
 });
@@ -52,86 +46,103 @@ document.getElementById('closeBtn').addEventListener('click', (e) => {
 
 // list functions
 
-function newList() {
+function addTab(counter) {
 
-    var name = document.getElementById('nameInput').value;
-    var note = document.getElementById('noteInput').value;
-    var date = document.getElementById('dateInput').value.replace(/-/g, '\/');
-    var priority = document.querySelector('input[name="priorityInput"]:checked').value;
+    let name = document.getElementById('nameInput').value;
+    let note = document.getElementById('noteInput').value;
+    let date = document.getElementById('dateInput').value.replace(/-/g, '\/');
+    let formattedDate = 'Due: ' + format(date, 'PP');
+    let priority = document.querySelector('input[name="priorityInput"]:checked').value;
 
-    let list = new List(name, note, date, priority);
+    const tabContent = document.createElement('div');
+    tabContent.classList = 'tab-content';
+    tabContent.setAttribute('id', 'list-' + counter);
 
     const listName = document.createElement('h2');
-    listName.textContent = list.name;
-    listName.id = 'listName';
-    main.appendChild(listName);
+    listName.textContent = name;
+    tabContent.appendChild(listName);
 
     const listNote = document.createElement('p');
-    listNote.textContent = list.note;
-    main.appendChild(listNote);
+    listNote.textContent = note;
+    tabContent.appendChild(listNote);
 
     const listDate = document.createElement('p');
-    listDate.textContent = list.date;
-    main.appendChild(listDate);
+    listDate.textContent = formattedDate;
+    tabContent.appendChild(listDate);
 
     const listPriority = document.createElement('p');
-    listPriority.textContent = list.priority;
-    main.appendChild(listPriority);
+    listPriority.textContent = priority;
+    tabContent.appendChild(listPriority);
 
-    const addDiv = document.createElement('div');
-    addDiv.id = 'addDiv';
+    listContainer.appendChild(tabContent);
+};
+
+function addList(counter) {
+
+    const tabContent = document.getElementById('list-' + counter);
+
+    const addDiv = document.createElement('addDiv');
     const addInput = document.createElement('input');
     addInput.type = 'text';
-    addInput.id = 'addInput';
+    addInput.setAttribute('id', 'addInput-' + counter)
     addDiv.appendChild(addInput);
 
     const addBtn = document.createElement('button');
     addBtn.type = 'submit';
     addBtn.innerHTML = 'add'
-    addBtn.addEventListener('click', newListItem);
+    addBtn.addEventListener('click', newListItem(counter));
     addDiv.appendChild(addBtn);
-
-    main.appendChild(addDiv);
+    tabContent.appendChild(addDiv);
 
     const ul = document.createElement('ul');
     ul.id = 'myList';
-    main.appendChild(ul);
-}
+    tabContent.appendChild(ul);
+    listContainer.appendChild(tabContent);
+};
 
-function addToSidebar() {
+function addToSidebar(counter) {
+    
+    const sidebarUl = document.getElementById('sidebarUl');
     const li = document.createElement('button');
+    li.classList = 'tab-link';
+    li.setAttribute('id', counter)
     li.innerHTML = document.getElementById('nameInput').value;
+    li.addEventListener('click', (e) => {
+        openTab(e.target.id);
+        });
     sidebarUl.appendChild(li);
     sidebar.appendChild(sidebarUl);
-}
+};
 
 // Create a new list item when clicking on the 'Add' button
 
-function newListItem() {
-    let li = document.createElement('li');
-    let inputValue = document.getElementById('addInput').value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert('You must write something!');
-      } else {
-        document.getElementById('myList').appendChild(li);
-      }
-      document.getElementById('addInput').value = '';
+function newListItem(counter) {
+    console.log(counter);
 
-      let span = document.createElement('span');
-      let txt = document.createTextNode('\u00D7');
-      span.className = 'close';
-      span.appendChild(txt);
-      li.appendChild(span);
+    // let li = document.createElement('li');
+    // let inputValue = document.getElementById(counter).value;
+    // let t = document.createTextNode(inputValue);
+    // li.appendChild(t);
+    // if (inputValue === '') {
+    //     alert('You must write something!');
+    //   } else {
+    //     document.getElementById('myList').appendChild(li);
+    //   }
+    //   document.getElementById(counter).value = '';
+
+    //   let span = document.createElement('span');
+    //   let txt = document.createTextNode('\u00D7');
+    //   span.className = 'close';
+    //   span.appendChild(txt);
+    //   li.appendChild(span);
     
-      for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-          let div = this.parentElement;
-          div.style.display = 'none';
-        }
-    }
-}
+    //   for (i = 0; i < close.length; i++) {
+    //     close[i].onclick = function() {
+    //       let div = this.parentElement;
+    //       div.style.display = 'none';
+    //     }
+    // }
+};
 
 // Create a 'close' button and append it to each list item
 let myNodelist = document.getElementsByTagName('li');
@@ -142,7 +153,7 @@ for (i = 0; i < myNodelist.length; i++) {
     span.className = 'close';
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
-    }
+    };
 
 // Click on a close button to hide the current list item
 let close = document.getElementsByClassName('close');
@@ -152,4 +163,24 @@ for (i = 0; i < close.length; i++) {
     let div = this.parentElement;
     div.style.display = 'none';
     }
+};
+
+// tab management
+
+function openTab(counter) { 
+
+    let activeList = 'list-' + counter;
+
+    const tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+    }
+
+    const tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(activeList).style.display = "block";
+    // evt.currentTarget.className += " active";
 };
