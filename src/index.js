fecc8b36895553e13bc7adf5798b14f986c50c94
Bex-G <bexgiacinto@gmail.com>
2024-05-19 18:26:38 -0700

    Remove form functions for clarity and add basic styling.

--- src/index.js
+++ src/index.js
@@ -1,215 +1,219 @@
-import {format} from 'date-fns';
+// import {format} from "date-fns";
 
-const sidebar = document.getElementById('sidebar');
-const listContainer = document.getElementById('listContainer');
+// const { intlFormat } = require("date-fns");
 
-document.getElementById('newListBtn').addEventListener('click', (e) => {
-    e.preventDefault();
-    openForm();
-});
+const sidebar = document.getElementById("sidebar");
+const tabContainer = document.getElementById("tabContainer");
 
-// FORM FUNCTIONS
-
-function openForm() {
-    document.getElementById("myForm").style.display = 'block';
-};
-
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-};
-
-function resetForm() {
-    document.getElementById('nameInput').value = null;
-    document.getElementById('dateInput').value = null;
-};
-
-// FORM EVENTS
-
-let n = 0; // variable used to keep track of list/tab id numbers
-
-document.getElementById('submitBtn').addEventListener('click', (e) => {
-    e.preventDefault();
-    if (validateForm() != false){
+document.getElementById("newListBtn").addEventListener("click", (e) => {
     n++
     addTab(n);
     addList(n);
     addToSidebar(n);
     openTab(n);
-    resetForm();
-    closeForm();
-    }
-});
+})
 
-document.getElementById('closeBtn').addEventListener('click', (e) => {
-    e.preventDefault();
-    resetForm();
-    closeForm();
-});
-
-// TAB + LIST FUNCTIONS
-
-function validateForm() {
-
-    let name = document.getElementById('nameInput').value;
-    if (name == "") {
-        alert("List name is required!")
-        return false;
-    }
-};
+let n = 0; // variable used to keep track of list/tab id numbers
 
 function addTab(n) {
 
-    let name = document.getElementById('nameInput').value;
-    let date = document.getElementById('dateInput').value;
+    let tabContent = document.createElement("div");
+    tabContent.classList = "tab-content";
+    tabContent.id = ("tab-" + n);
 
-    let tabContent = document.createElement('div');
-    tabContent.classList = 'tab-content';
-    tabContent.id = ('tab-' + n);
+    let info = document.createElement("div");
+    info.classList = "list-info";
 
-    let tabHead = document.createElement('div');
-    tabHead.classList = 'tab-head';
+    let header = document.createElement("div");
+    header.classList = "tab-header";
 
-    let listName = document.createElement('h2');
-    listName.textContent = name;
-    tabHead.appendChild(listName); 
+    let listName = document.createElement("h1");
+    listName.textContent = ("Quest " + n);
+    listName.id = ("name" + n);
+    listName.contentEditable = "true";
+    listName.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            listName.blur();
+            document.getElementById(n).textContent = (listName.textContent.length <= 12) 
+            ? listName.textContent 
+            : (listName.textContent.substring(0,12)) + "...";
+        }
+    })
+    listName.addEventListener("focusout", () => {
+        document.getElementById(n).textContent = (listName.textContent.length <= 12) 
+        ? listName.textContent 
+        : (listName.textContent.substring(0,12)) + "...";
+    })
+    header.appendChild(listName); 
 
-    let editBtn = document.createElement('button');
-    editBtn.innerHTML = '...';
-    editBtn.addEventListener('click', (e) => {
-        openForm(); // this isn't right... maybe it needs to be a different form completely?
-        document.getElementById('nameInput').value = name;
-        document.getElementById('dateInput').value = date;
-        });
-    tabHead.appendChild(editBtn);
-    tabContent.appendChild(tabHead);
+    let r = document.createElement("div");
+    r.id = "headerR";
 
-    let dateLine = document.createElement('div');
-    dateLine.classList = ('date-line');
-
-    let altDate = date.replace(/-/g, '\/');
-    if (date != ""){
-        let dueDate = document.createElement('p');
-        dueDate.textContent = 'due: ' + format(altDate, 'PP');
-        dateLine.appendChild(dueDate);
-    };
-
-    let label = document.createElement('label');
-    label.for = 'priorityInput';
-    label.innerHTML = 'main quest?'
-    dateLine.appendChild(label);
-    let priorityInput = document.createElement('input');
-    priorityInput.type = 'checkbox';
-    priorityInput.id = 'priorityInput';
-    priorityInput.addEventListener('change' , (e) => {
-        tabContent.classList.toggle('important');
+    let removeBtn = document.createElement("button");
+    removeBtn.id = "removeBtn";
+    let txt = document.createTextNode("\u00D7");
+    removeBtn.appendChild(txt);
+    removeBtn.addEventListener("click", () => {
+        let result = confirm("Are you sure you want to delete this list? (This can not be undone.)")
+        if (result === true) {
+        document.getElementById("tab-" + n).remove();
+        document.getElementById(n).remove();
+        }
     });
-    dateLine.appendChild(priorityInput);
-    tabContent.appendChild(dateLine);
+    r.appendChild(removeBtn);
 
-    let notes = document.createElement('p');
-    notes.textContent = 'notes:';
-    notes.contentEditable = 'true';
-    tabContent.appendChild(notes);
+    header.appendChild(r);
+    info.appendChild(header);
 
-    listContainer.appendChild(tabContent);
-};
+    let p = document.createElement("div");
+    p.id = "priority";
+    let pL = document.createElement("label");
+    pL.htmlFor = ("priorityInput" + n);
+    pL.innerHTML = "main quest?"
+    p.appendChild(pL);
+    let priorityInput = document.createElement("input");
+    priorityInput.type = "checkbox";
+    priorityInput.id = ("priorityInput" + n);
+    priorityInput.addEventListener("change" , (e) => {
+        info.classList.toggle("important");
+    });
+    p.appendChild(priorityInput);
+
+    let d = document.createElement("div");
+    d.id = "date";
+    let dL = document.createElement("label");
+    dL.htmlFor = ("dateInput" + n);
+    dL.innerHTML = "due by: "
+    d.appendChild(dL);
+    let dateInput = document.createElement("input");
+    dateInput.type = "date";
+    dateInput.id = ("dateInput" + n);
+    d.appendChild(dateInput);
+
+    let notes = document.createElement("p");
+    notes.id = "notes";
+    notes.textContent = "notes:";
+    notes.contentEditable = "true";
+
+    info.appendChild(p);
+    info.appendChild(d);
+    info.appendChild(notes);
+    tabContent.appendChild(info);
+    tabContainer.appendChild(tabContent);
+}
+
+// function formatDate() {
+//     let date = document.getElementById("dateInput" + n).value.replace(/-/g, "\/");
+// }
 
 function addList(n) {
 
-    let tabContent = document.getElementById('tab-' + n);
+    let tabContent = document.getElementById("tab-" + n);
 
-    let div = document.createElement('div');
-    div.id = ('addDiv');
-    let addInput = document.createElement('input');
-    addInput.type = 'text';
-    addInput.id = ('addInput-' + n);
+    let div = document.createElement("div");
+    let addInput = document.createElement("input");
+    addInput.type = "text";
+    addInput.id = ("addInput-" + n);
+    addInput.classList = "addInput";
+    addInput.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            newListItem(e.target.id);
+        }
+    });
     div.appendChild(addInput);
 
-    let addBtn = document.createElement('button');
-    addBtn.type = 'submit';
-    addBtn.innerHTML = 'add'
-    addBtn.id = ('addBtn-' + n);
-    addBtn.addEventListener('click', (e) => {
+    let addBtn = document.createElement("button");
+    addBtn.type = "submit";
+    addBtn.innerHTML = "add"
+    addBtn.id = ("addBtn-" + n);
+    addBtn.addEventListener("click", (e) => {
         newListItem(e.target.id);
         });
     div.appendChild(addBtn);
     tabContent.appendChild(div);
 
-    let ul = document.createElement('ul');
-    ul.id = ('list-' + n)
+    let ul = document.createElement("ul");
+    ul.id = ("list-" + n)
+    ul.classList = "listUl";
     tabContent.appendChild(ul);
-    listContainer.appendChild(tabContent);
-};
+    tabContainer.appendChild(tabContent);
+}
 
 function addToSidebar(n) {
     
-    let sidebarUl = document.getElementById('sidebarUl');
+    let sidebarUl = document.getElementById("sidebarUl");
 
-    let li = document.createElement('button');
-    li.classList = 'tabBtn';
+    let li = document.createElement("button");
+    li.classList = "tab-btn";
     li.id =  (n);
-    li.innerHTML = document.getElementById('nameInput').value;
+    li.innerHTML = ("Quest " + n);
 
-    li.addEventListener('click', (e) => {
-        openTab(e.target.id); // gets correct 'n' value
+    li.addEventListener("click", (e) => {
+        openTab(e.target.id); // gets correct "n" value
         });
     sidebarUl.appendChild(li);
     sidebar.appendChild(sidebarUl);
-};
+}
 
-// create new li with 'add' button
+// create new li with "add" button
 
 function newListItem(event) {
 
-    let index = event.indexOf('-');
+    let index = event.indexOf("-");
     let n = event.substring(index + 1);
 
-    let li = document.createElement('li');
-    let inputValue = document.getElementById('addInput-' + n).value;
-    let t = document.createTextNode(inputValue);
+    let li = document.createElement("li");
+    let input = document.getElementById("addInput-" + n).value;
+    let t = document.createTextNode(input);
     li.appendChild(t);
 
-    if (inputValue === '') {
-        alert('You must write something!');
+    if (input === "") {
+        alert("You must write something!");
         } else {
-        document.getElementById('list-' + n).appendChild(li);
+        document.getElementById("list-" + n).appendChild(li);
         }
-        document.getElementById('addInput-' + n).value = '';
 
-        let span = document.createElement('span');
-        let txt = document.createTextNode('\u00D7');
-        span.classList = 'close';
-        span.appendChild(txt);
-        li.appendChild(span);
+    document.getElementById("addInput-" + n).value = "";
+
+    let span = document.createElement("span");
+    span.classList = "close";
+    let txt = document.createTextNode("\u00D7");
+    span.appendChild(txt);
+    span.addEventListener("mouseover", () => {
+        li.classList.toggle("hover");
+    });
+    span.addEventListener("mouseout", () => {
+        li.classList.toggle("hover");
+    });
+    li.appendChild(span);
     
-      for (i = 0; i < close.length; i++) {
+    for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
-          let div = this.parentElement;
-          div.style.display = 'none';
-        }
-    }
-};
+        let div = this.parentElement;
+        div.style.display = "none";
+    }}
+}
 
-// append 'close' button to each li
-let myNodelist = document.getElementsByTagName('li');
+// append "close" button to each li
+let myNodelist = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement('span');
-    let txt = document.createTextNode('\u00D7');
-    span.classList = 'close';
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.classList = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
-    };
+    }
 
 // enable close button to hide li
-let close = document.getElementsByClassName('close');
+let close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
     let div = this.parentElement;
-    div.style.display = 'none';
+    div.style.display = "none";
     }
-};
+}
 
 // TAB MANAGEMENT FUNCTIONS
 
@@ -217,22 +221,28 @@ function openTab(n) {
 
     // hide all tab content, then display active tab
 
-    let tabContent = document.getElementsByClassName('tab-content');
+    let tabContent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = 'none';
+    tabContent[i].style.display = "none";
     };
-    document.getElementById('tab-' + n).style.display = 'block';
+    document.getElementById("tab-" + n).style.display = "block";
 
-    // remove '.active' from all tabs, then make tab n 'active'
+    // remove ".active" from all tabs, then make tab n "active"
 
-    let els = document.querySelectorAll('.tabBtn');
+    let els = document.querySelectorAll(".tab-btn");
     for (i = 0; i < els.length; i++) {
-        els[i].classList.remove('active')
+        els[i].classList.remove("active")
     };
-    document.getElementById(n).classList.add('active');
+    document.getElementById(n).classList.add("active");
 
     // clear input values when switching between tabs
     
-    let addInput = 'addInput-' + n;
+    let addInput = "addInput-" + n;
     document.getElementById(addInput).value = null;
+
+    // manage form
+
+    // if (form.classList.contains("open")) {
+    // closeForm();
+    // };
 };
